2b9e1ba3bc0a09256e81e96ce9f5f10d31b5c4f2
Anonymous <rahulking25102006@gmail.com>
2022-03-07 02:41:18 +0530

    Update stream.py

--- YukkiMusic/plugins/play/stream.py
+++ YukkiMusic/plugins/play/stream.py
@@ -27,10 +27,12 @@ async def stream_command(
     video,
     channel,
     playmode,
-    mystic,
     url,
 ):
     if url:
+        mystic = await message.reply_text(
+            _["play_2"].format(channel) if channel else _["play_1"]
+        )
         try:
             await Yukki.stream_call(url)
         except NoActiveGroupCall:
